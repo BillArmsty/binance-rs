7d3f0830687d3ac8a65c5a316917a299e927d339
Bill Armstrong <armsty9@gmail.com>
2023-05-24 17:28:44 +0300

    Able to get the raw stream data

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use url::Url;
 
 static BINANCE_WS_API: &str = "wss://stream.binance.com:9443";
 fn main() {
-    let binance_url = format!("{}/ws", BINANCE_WS_API);
+    //Get top 5 bids and asks for ETHBTC every 100ms
+    let binance_url = format!("{}/stream?streams={}", BINANCE_WS_API, "ethbtc@depth@100ms");
+    
     let (mut socket, response) = connect(Url::parse(&binance_url).unwrap()).expect("Can't connect");
 
     println!("Connected to binance stream");
@@ -15,8 +17,18 @@ fn main() {
         println!("* {}: {:?}", header, _value);
     }
 
+
+    //Process messages in an infinite loop using serde
     loop {
         let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
+        let msg = match msg {
+            tungstenite::Message::Text(s) => s,
+            _ => {
+                panic!("Error getting text");
+            }
+        };
+
+        let parsed_data: serde_json::Value = serde_json::from_str(&msg).expect("Unable to parse the message");
+        println!("{:?}", parsed_data);
     }
 }
